cb7208409775f4883683500020af242370d26691
Artem Tkachenko <tomsoir@gmail.com>
2017-05-18 17:46:18 -0700

    Design of new preloader

--- src/app/components/LoadingXpromo/index.jsx
+++ src/app/components/LoadingXpromo/index.jsx
@@ -6,15 +6,22 @@ import { createSelector } from 'reselect';
 import { isXPromoAdLoadingEnabled } from 'app/selectors/xpromo';
 import AppButton from 'app/components/DualPartInterstitial/AppButton';
 import Loading from 'app/components/Loading';
+import SnooIcon from 'app/components/SnooIcon';
 
 const AdLoading = () => {
   return (
     <div className='adLoading'>
-      <Loading />
-      <p>Happy text here</p>
+      <div className='adLoading__logo'>
+        <SnooIcon />
+        <div className='adLoading__logoBg'></div>
+      </div>
+
+      <h3>The Reddit App is&nbsp;50% Faster than&nbsp;Web</h3>
+      <p>Infinite Scrolls. Autoplay GIFs.</p>
+
       <div className='adLoading__wrapper'>
         <div className='adLoading__button'>
-          <AppButton title='Get your App'/>
+          <AppButton title='UPGRADE TO APP'/>
         </div>
       </div>
     </div>
