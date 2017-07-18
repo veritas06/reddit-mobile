7fb285893d342702a565684dbae17c505c75eb85
Saurabh Sharma <saurabh.sharma@reddit.com>
2017-07-18 16:53:24 -0700

    fix

--- github-comment.rb
+++ github-comment.rb
@@ -2,6 +2,8 @@ require 'octokit'
 client = Octokit::Client.new(:access_token => ENV["GITHUB_ACCESS_TOKEN"])
 repo = ENV["GITHUB_REPO"]
 deployment_url = ENV["DEPLOYMENT_URL"]
+puts deployment_url
 branch_name = ENV["GIT_BRANCH"]
 pr_number = client.get(deployment_url)[:payload][:pull_request_number]
+puts pr_number
 client.add_comment(repo, pr_number, "### ✅ Branch was successfully deployed! 🎉\n- [Your branch](http://#{branch_name}.mobile.staging.snooguts.net) is available here 👉 http://#{branch_name}.mobile.staging.snooguts.net\n- All deployed branches [here](http://branches.mobile.staging.snooguts.net)")
