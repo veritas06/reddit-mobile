cdd7f48a38968f8639e2b250f1114579a60ab188
Saurabh Sharma <saurabh.sharma@reddit.com>
2017-07-18 15:04:27 -0700

    add comment on success

---
+++ github-deployment.rb
@@ -0,0 +1,7 @@
+require 'octokit'
+client = Octokit::Client.new(:access_token => ENV["GITHUB_ACCESS_TOKEN"])
+repo = ENV["GITHUB_REPO"]
+ref = ENV["GIT_REF"]
+pr_number = ENV["GITHUB_PULL_REQUEST_NUMBER"]
+branch_name = ENV["GIT_BRANCH"]
+deployment = client.create_deployment(repo, ref, {environment: "staging", required_contexts:[], payload: { pull_request_number: pr_number}})
