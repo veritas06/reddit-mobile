a8794905e3c7e8d938014d96dd0ed162bcb8455e
Saurabh Sharma <saurabh.sharma@reddit.com>
2017-07-18 16:57:38 -0700

    fix

--- github-comment.rb
+++ github-comment.rb
@@ -7,4 +7,4 @@ branch_name = ENV["GIT_BRANCH"]
 pr_number = client.get(deployment_url)[:payload][:pull_request_number]
 puts pr_number.inspect
 sleep 1
-client.add_comment(repo, pr_number, "### ✅ Branch was successfully deployed! 🎉\n- [Your branch](http://#{branch_name}.mobile.staging.snooguts.net) is available here 👉 http://#{branch_name}.mobile.staging.snooguts.net\n- All deployed branches [here](http://branches.mobile.staging.snooguts.net)")
+client.add_comment(repo, pr_number, "### ✅ Branch was successfully deployed! 🎉\n- [Your branch](http://#{branch_name}.mobile.staging.snooguts.net) is available here 👉 http://#{branch_name}.mobile.staging.snooguts.net\n- You can see all deployed branches [here](http://branches.mobile.staging.snooguts.net)")
